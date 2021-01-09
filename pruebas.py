5505f75166d64272bb38df5a54558db8fba4329c
Carlos <alu0101102726@ull.edu.es>
2021-01-09 20:53:34 +0000

    Refactorizando

--- pruebas.py
+++
@@ -1,9 +0,0 @@
-import igraph
-from igraph import *
-
-g = Graph()
-g.add_vertices(6)
-g.add_edges([(0,1), (1,2), (2,0), (2, 3), (3, 4), (4, 5), (5, 3)])
-print(g)
-layout = g.layout("kk")
-plot(g, layout = layout)
