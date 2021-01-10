73fcd388b9b0639ca2d9cd36473af89f0f50cef5
alu0101121529 <alu0101121529@ull.edu.es>
2021-01-10 12:14:27 +0000

    primera visualización del grafo

---
+++ graph_transformation.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from vertex_cover import *
+from edge import *
+from graph import *
+from vertex import *
+
+class graph_transformation:
+  def __init__(self, graph):
+    self.__original_graph = graph
+    self.__networkx_graph = nx.Graph()
+    super().__init__()
+  
+  def build_nodes(self):
+    self.__networkx_graph.add_nodes_from(self.__original_graph.vertices)
+    
+  def build_edges(self):
+    networkx_edges = []
+    original_edges = self.__original_graph.get_edges()
+    for edge in original_edges:
+      #dummy_edge = ()
+      dummy_edge= (edge.start_vertex.id, edge.end_vertex.id)
+      networkx_edges.append(dummy_edge)
+    self.__networkx_graph.add_edges_from(networkx_edges)
+    
+  def print_graph(self):
+    self.build_nodes()
+    self.build_edges()
+    print(list(self.__networkx_graph.nodes), "\n")
+    print(list(self.__networkx_graph.edges))
+    pos=nx.spring_layout(self.__networkx_graph)
+    #plt.subplot(121)
+    nx.draw(self.__networkx_graph, pos, with_labels = True)
+    plt.show()
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from graph import *
 from vertex import *
 from vertex_cover import *
 from fileManager import *
+from graph_transformation import *
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 three_sat = ThreeSAT()
 vtx_cover = VertexCover(three_sat)
@@ -12,4 +16,16 @@ file_loader = FileManager(three_sat, "file.sat3")
 file_loader.loadFile()
 print(three_sat.to_string())
 vtx_cover.build_vc()
-vtx_cover.print_vc()
+#vtx_cover.print_vc()
+visual_graph = graph_transformation(vtx_cover.graph)
+visual_graph.print_graph()
+
+
+# original_graph = vtx_cover.graph
+
+# G = nx.Graph()
+# G.add_nodes_from(original_graph.vertices)
+# G.add_edges_from(original_graph.get_edges()) 
+# plt.subplot(122)
+# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+# plt.show()
