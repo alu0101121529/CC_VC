5144b95e5ccc5501182f757aa4045ae137c76a48
alu0101121529 <alu0101121529@ull.edu.es>
2021-01-10 12:23:04 +0000

    embelleciendo pelotas

--- graph_transformation.py
+++ graph_transformation.py
@@ -18,7 +18,6 @@ class graph_transformation:
     networkx_edges = []
     original_edges = self.__original_graph.get_edges()
     for edge in original_edges:
-      #dummy_edge = ()
       dummy_edge= (edge.start_vertex.id, edge.end_vertex.id)
       networkx_edges.append(dummy_edge)
     self.__networkx_graph.add_edges_from(networkx_edges)
@@ -26,9 +25,6 @@ class graph_transformation:
   def print_graph(self):
     self.build_nodes()
     self.build_edges()
-    print(list(self.__networkx_graph.nodes), "\n")
-    print(list(self.__networkx_graph.edges))
     pos=nx.spring_layout(self.__networkx_graph)
-    #plt.subplot(121)
-    nx.draw(self.__networkx_graph, pos, with_labels = True)
+    nx.draw(self.__networkx_graph, pos, with_labels = True, node_size = 800, font_size = 8)
     plt.show()
